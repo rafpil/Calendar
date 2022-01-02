04adffc946eb255293e967f44d86baac15774236
Rafal <Rafal>
2022-01-02 21:28:34 +0100

    Added MDB Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { MDBBadge } from 'mdb-react-ui-kit';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <MDBBadge>Primary</MDBBadge>
+      <MDBBadge className='mx-2' color='secondary'>
+        Secondary
+      </MDBBadge>
+      <MDBBadge color='success'>Success</MDBBadge>
+      <MDBBadge className='mx-2' color='danger'>
+        Danger
+      </MDBBadge>
+      <MDBBadge color='warning'>Warning</MDBBadge>
+      <MDBBadge className='mx-2' color='info'>
+        Info
+      </MDBBadge>
+      <MDBBadge className='text-dark me-2' color='light'>
+        Light
+      </MDBBadge>
+      <MDBBadge color='dark'>Dark</MDBBadge>
       </header>
     </div>
   );
