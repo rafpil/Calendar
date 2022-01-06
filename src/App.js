8ea6b0d9c25be261873829a5d4a96e857db2209f
Rafal <Rafal>
2022-01-06 12:50:08 +0100

    Added col style and changed css to sass file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import LgCalendar from "./Components/LargeCalendar/CalendarLg";
 import Sidebar from "./Components/Sidebar/Sidebar";
 
